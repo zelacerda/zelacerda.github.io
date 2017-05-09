7e3a80fcc08bef432a16d01734f927daab541119
José Alexandre Costa de Lacerda <jcl@certi.org>
2017-05-09 11:25:08 -0300

    Bouncing ball improved

--- p5/1/sketch.js
+++ p5/1/sketch.js
@@ -1,32 +1,41 @@
 function setup() {
-  createCanvas(windowWidth, windowHeight);
-  //frameRate(60);
-  //background(20, 20, 20);
-  fill(0);
-  posX = windowWidth / 2;
-  posY = windowHeight /2;
-  velX = 1;
-  velY = 1;
-  blendMode(REPLACE);
+    createCanvas(windowWidth, windowHeight);
+    fill(255);
+    bola = new Bola(50);  
 }
 
 function windowResized() {
-  resizeCanvas(windowWidth, windowHeight);
-  //background(20, 20, 20);
+    resizeCanvas(windowWidth, windowHeight);
+    //background(20, 20, 20);
 }
 
 function draw() {
-  ellipse(posX, posY, 80, 80);
-  move();
+    background(51);
+    bola.move();
+    bola.draw();
 }
 
-function move() {
-  posX += velX;
-  if (posX > windowWidth) {
-    velX *= -1;
-  }
-  posY += velY;
-  if (posY > windowHeight) {
-    velY *= -1;
-  }
+// The Bola class
+function Bola(size) {
+    this.size = size;
+    this.x = windowWidth / 2;
+    this.y = windowHeight / 2;
+    this.xVel = random(-3, 3);
+    this.yVel = random(-3, 3);
+    
+    this.move = function() {
+        this.x += this.xVel;
+        if (this.x > windowWidth - this.size/2 || this.x < this.size/2) {
+            this.xVel *= -1;
+        }
+        
+        this.y += this.yVel;
+        if (this.y > windowHeight - this.size/2 || this.y < this.size/2) {
+            this.yVel *= -1;
+        }
+        
+    this.draw = function() {
+        ellipse(this.x, this.y, this.size, this.size)
+    }
+    }
 }
--- p5/particle/sketch.js
+++ p5/particle/sketch.js
@@ -14,7 +14,7 @@ function draw() {
 // A simple Particle class
 var Particle = function(position) {
   this.acceleration = createVector(0, 0.05);
-  this.velocity = createVector(random(-1, 1), random(-1, 0));
+  this.velocity = createVector(random(-1, 1), random(-3, 0));
   this.position = position.copy();
   this.lifespan = 255.0;
 };
