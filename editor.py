8d5d36780c6cd518b80d5ada4a614eca79c43ffd
José Alexandre Costa de Lacerda <jcl@certi.org.br>
2020-06-14 00:07:07 -0300

    Add markdown editor experiment

---
+++ editor.py
@@ -0,0 +1,27 @@
+default = '''
+Editor Markdown
+===============
+
+*2020 - by zelacerda*
+
+### Um editor de texto simples usando Brython.
+
+Este experimento utiliza o módulo **markdown** do 
+[Brython](https://brython.info/) para converter um texto no formato 
+markdown em HTML e exibir o resultado na própria página.
+
+É possível anotar `codigo` ou mesmo incluir blocos como este:
+
+```
+def sum(a, b):
+    return a + b
+```
+
+Ideias pra melhorar este experimento:
+
+* Permitir salvar e recuperar texto usando `localStorage` do HTML5;
+* Verificar se é possível implementar *Syntax highlighting*;
+* Emojis :)
+
+**Exiba o código fonte desta página para ver com o Brython funciona!**
+'''
