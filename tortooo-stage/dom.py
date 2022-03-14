b3fed23f8fbdf9416bc942156a5bb56582316597
José Alexandre Costa de Lacerda <zelacerda@gmail.com>
2022-03-14 13:15:13 -0300

    [tort.ooo] CSS tests

---
+++ tortooo-stage/dom.py
@@ -0,0 +1,18 @@
+from browser.html import DIV
+from browser import document
+
+
+class Div(DIV):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+    def append(elt):
+        super() <= elt
+
+
+class Page():
+    def __init__(self):
+        self.dom = document
+
+    def append(self, elt):
+        self.dom <= elt
